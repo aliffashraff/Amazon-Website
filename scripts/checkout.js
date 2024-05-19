efff4ece924980c45b94ef38218efac2878806d6
Aliff Ashraff <aliffzulkiffli97@gmail.com>
2024-05-19 17:02:38 +0800

    create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import{renderPaymentSummary} from './checkout/paymentSummary.js';
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
-import '../data/cart-oop.js'; //import all from the file
+import '../data/cart-class.js'; //import all from the file
 
 renderCheckoutHeader();
 renderOrderSummary();
