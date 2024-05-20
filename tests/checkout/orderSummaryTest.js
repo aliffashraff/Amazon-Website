79f8162ea589b0996c6a7929febe3b83d440dca4
Aliff Ashraff <aliffzulkiffli97@gmail.com>
2024-05-20 18:56:13 +0800

    load products in before alll hooks

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,10 +1,20 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import {loadfromStorage, cart} from "../../data/cart.js";
+import {loadProducts} from "../../data/products.js";
 
 
  describe('test suite: renderOrderSummary', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+  //run function before all of the tests
+  beforeAll((done) => {
+    // done() is function provided by Jasmine - to control when to go to the next step
+    loadProducts(() => {
+      done();
+    });
+  })
+
   // beforeEach hook - before each of test will run this code
   beforeEach(() => {
     spyOn(localStorage, 'setItem');
